a377e88c7d30a5b9241b64b7c8fa2bcbcb01cd35
Iovannysm <oenjad@gmail.com>
2018-05-28 03:09:33 +0000

    Create User, username, email fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
-class User < ApplicationRecord
-  
+class User < ActiveRecord::Base
+   validates :username, presence: true, length: { minimum: 2, maximum: 20 }
+    validates :email, presence: true, length: { minimum: 2, maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516003752) do
+ActiveRecord::Schema.define(version: 20180521081255) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180516003752) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
