937560b8a1b6440705b3fefb4325b8e5ca3d0d94
Iovannysm <oenjad@gmail.com>
2018-05-31 13:58:49 +0000

    Add password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531091758) do
+ActiveRecord::Schema.define(version: 20180531135502) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
