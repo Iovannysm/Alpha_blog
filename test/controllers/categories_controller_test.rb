64d9b755906e41d986cea3d0db50750c096a347b
Iovannysm <oenjad@gmail.com>
2019-02-06 16:45:02 +0000

    Add categories, navigation and addmin only categories privilage

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "sports")
+     @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
   
   test "should get catergories index" do
@@ -11,12 +12,19 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
-    get :new
+    sign_in_as(@user, "password")
+    get new_category_path
     assert_response :success
   end
   
   test "should get show" do
     get :show, params: {id: @category.id}
   end
-  
+   
+   test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post(:create, params: { category: { name: "sports"}})
+    end
+    assert_redirected_to categories_path
+  end
 end
